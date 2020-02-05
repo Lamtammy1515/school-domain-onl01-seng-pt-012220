e560f50d9542251963234588113e9c24be35a7cc
Tammy Lam <lamtammy1515@gmail.com>
2020-02-05 23:45:12 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+
+class School
+  attr_accessor :name 
+
+  def initialize(name)
+    @name = name 
+    @roster = {}
+  end 
+  
+  def roster
+    @roster
+  end 
+  
+  def add_student(student, grade)
+    @student = student
+    @grade = grade
+  end
+  
+  def add_student
+    @student
+    @grade
+  end
+end 
+
