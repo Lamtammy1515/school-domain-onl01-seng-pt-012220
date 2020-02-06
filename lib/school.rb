3c88e78b1c0a8949ceedb490c077c783269ca7f5
Tammy Lam <lamtammy1515@gmail.com>
2020-02-06 00:01:45 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -13,7 +13,11 @@ class School
   end 
   
   def add_student(name, grade)
-    
+    if @roster[grade]
+      @roster[grade] << name 
+    else 
+      @roster[grade] = name 
+    end 
   end
   
   def add_student
